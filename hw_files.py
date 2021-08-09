232647b8ce3f8082d423f99e3498feef313d125b
Olesya Zotova <kvanry777@gmail.com>
2021-08-10 00:25:00 +0300

    добавила_pprint

--- hw_files.py
+++ hw_files.py
@@ -1,20 +1,20 @@
-
+from pprint import pprint
 with open ("recipes.txt", "r", encoding="utf-8") as file:
-    dic = {}
+    cook_book = {}
     for line in file:
         dish = line.strip()
-        print(dish)
-        dic[dish] = []
+        # print(dish)
+        cook_book[dish] = []
         ingredients = int(file.readline().strip())
-        print(ingredients)
+        # print(ingredients)
         for i in range(ingredients):
             str = file.readline().strip()
             str_sp = str.split(" | ")
-            print(str_sp)
-            dic[dish] = [{'ingredient_name': str_sp[0], 'quantity': str_sp[1], 'measure': str_sp[2]}]
+            # print(str_sp)
+            cook_book[dish] = [{'ingredient_name': str_sp[0], 'quantity': str_sp[1], 'measure': str_sp[2]}]
         file.readline()
         print()
-    print(dic)
+    pprint(cook_book)
     
 
 
