e6d4b2acb64ae3e87dde2cc557b340ffaaad29b7
Olesya Zotova <kvanry777@gmail.com>
2021-08-10 00:11:48 +0300

    доработка_1дз

--- hw_files.py
+++ hw_files.py
@@ -1,26 +1,22 @@
 
 with open ("recipes.txt", "r", encoding="utf-8") as file:
+    dic = {}
     for line in file:
         dish = line.strip()
         print(dish)
+        dic[dish] = []
         ingredients = int(file.readline().strip())
+        print(ingredients)
         for i in range(ingredients):
-            print(file.readline().strip())
+            str = file.readline().strip()
+            str_sp = str.split(" | ")
+            print(str_sp)
+            dic[dish] = [{'ingredient_name': str_sp[0], 'quantity': str_sp[1], 'measure': str_sp[2]}]
         file.readline()
         print()
+    print(dic)
     
-    
-    
-    
-    
-# file = {}
-#     for line1 in file:                           # Loop over all lines in the file
-#         if line1.strip() == '':                # If the line is blank
-#             continue                          # Skip the blank line
-#         elif line1.startswith("group"):        # Else if line starts with group
-#             file = line1.strip()                # Strip whitespace and save key
-#             dic[file] = []                     # Initialize empty list
-#         else:
-#             dic[file].append(line1.split())     # Not key so append values
 
-# print (file)
+
+
+    
