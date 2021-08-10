98a4f921004bf887c1970a4f0716bc81fc8d8290
Olesya Zotova <kvanry777@gmail.com>
2021-08-11 00:09:30 +0300

    Дз1.1;1.2

--- hw_files.py
+++ hw_files.py
@@ -1,3 +1,4 @@
+from os import name
 from pprint import pprint
 with open ("recipes.txt", "r", encoding="utf-8") as file:
     cook_book = {}
@@ -11,11 +12,23 @@ with open ("recipes.txt", "r", encoding="utf-8") as file:
             str = file.readline().strip()
             str_sp = str.split(" | ")
             # print(str_sp)
-            cook_book[dish] = [{'ingredient_name': str_sp[0], 'quantity': str_sp[1], 'measure': str_sp[2]}]
+            cook_book[dish].append({'ingredient_name': str_sp[0], 'quantity': str_sp[1], 'measure': str_sp[2]})
         file.readline()
         print()
-    pprint(cook_book)
     
+# pprint(cook_book)
+
+def get_shoy_dishes(dic, dish, persons):
+    var = dic[dish]
+    cook = {}
+    for v in var:
+        cook[v['ingredient_name']] = {'quantity': int(v['quantity']) * persons, 'measure': v['measure']}
+    pprint(cook)       
+
+# get_shoy_dishes(cook_book, 'Запеченный картофель', 7)
+
+
+
 
 
 
